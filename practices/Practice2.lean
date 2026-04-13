053e24d6fdabbebda37826256ca110769e616469
pillyshi <pillyshi21@gmail.com>
2026-04-13 16:38:11 +0900

    Add practices.

---
+++ practices/Practice2.lean
@@ -0,0 +1,50 @@
+axiom Set : Type
+axiom Elem : Set -> Set -> Prop
+
+infix:50 " ∈ " => Elem
+
+-- Axiom
+
+axiom extensionality : ∀ x y : Set, (∀ z, z ∈ x ↔ z ∈ y) -> x = y
+
+axiom emptySet : ∃ e, ∀ x, ¬ x ∈ e
+noncomputable def empty : Set := emptySet.choose
+theorem empty_ax : ∀ x, ¬ x ∈ empty := emptySet.choose_spec
+
+axiom pairSet : ∀ x y : Set, ∃ p : Set, ∀ z, z ∈ p ↔ z = x ∨ z = y
+noncomputable def pair (x y : Set) : Set := (pairSet x y).choose
+theorem pair_ax (x y : Set) : ∀ z, z ∈ pair x y ↔ z = x ∨ z = y := (pairSet x y).choose_spec
+
+def subset (x y : Set) : Prop := ∀ z, z ∈ x → z ∈ y
+
+infix:50 " ⊆ " => subset
+
+-- Theorem
+
+theorem empty_unique : ∀ e₁ e₂, (∀ x, ¬ x ∈ e₁) -> (∀ x, ¬ x ∈ e₂) -> e₁ = e₂ := by
+  intro e₁ e₂ h₁ h₂
+  apply extensionality
+  intro z
+  constructor
+  · intro hz
+    exact absurd hz (h₁ z)
+  · intro hz
+    exact absurd hz (h₂ z)
+
+theorem pair_comm (x y : Set) : pair x y = pair y x := by
+  apply extensionality
+  intro z
+  rw [pair_ax, pair_ax]
+  constructor
+  · intro h
+    cases h with
+    | inl h => exact Or.inr h
+    | inr h => exact Or.inl h
+  · intro h
+    cases h with
+    | inl h => exact Or.inr h
+    | inr h => exact Or.inl h
+
+theorem subset_trans (x y z : Set) : x ⊆ y -> y ⊆ z -> x ⊆ z := by
+  intro h₁ h₂ a h₃
+  exact (h₂ a) ((h₁ a) h₃)
---
+++ practices/Practice3.lean
@@ -0,0 +1,49 @@
+axiom Set : Type
+axiom Elem : Set -> Set -> Prop
+
+infix:50 " ∈ " => Elem
+
+axiom extensinality : ∀ x y, (∀ w, w ∈ x ↔ w ∈ y) → x = y
+
+axiom empty_set : ∃ x, ∀ y, ¬ y ∈ x
+noncomputable def empty : Set := empty_set.choose
+theorem empty_ax : ∀ x, ¬ x ∈ empty := empty_set.choose_spec
+
+axiom pair_set : ∀ x y : Set, ∃ p : Set, ∀ z, z ∈ p ↔ z = x ∨ z = y
+noncomputable def pair (x y : Set) : Set := (pair_set x y).choose
+theorem pair_ax (x y : Set) : ∀ z, z ∈ pair x y ↔ z = x ∨ z = y := (pair_set x y).choose_spec
+
+def subset (x y : Set) : Prop := ∀ z, z ∈ x → z ∈ y
+
+infix:50 " ⊆ " => subset
+
+-- Theorem
+theorem empty_unique : ∀ e₁ e₂, (∀ x, ¬ x ∈ e₁) → (∀ x, ¬ x ∈ e₂) → e₁ = e₂ := by
+  intro e₁ e₂ h₁ h₂
+  apply extensinality
+  intro w
+  constructor
+  · intro hw
+    exact absurd hw (h₁ w)
+  · intro hw
+    exact absurd hw (h₂ w)
+
+theorem pair_comm (x y : Set) : pair x y = pair y x := by
+  apply extensinality
+  intro w
+  rw [pair_ax, pair_ax]
+  constructor
+  · intro h
+    cases h with
+    | inl h => exact Or.inr h
+    | inr h => exact Or.inl h
+  · intro h
+    cases h with
+    | inl h => exact Or.inr h
+    | inr h => exact Or.inl h
+
+theorem subset_trans (x y z : Set) : x ⊆ y → y ⊆ z → x ⊆ z := by
+  intro h₁ h₂ a h₃
+  -- h₁ a : a ∈ x → a ∈ y
+  -- h₂ a : a ∈ y → a ∈ z
+  exact  (h₂ a) ((h₁ a) h₃)
